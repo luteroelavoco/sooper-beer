32358e85f1cad5d147a9c552f60658bdc633470e
lutero.elavoco <lutero.elavoco@mutual.club>
2022-03-09 17:30:37 -0300

    fix: not found

--- src/components/List/index.tsx
+++ src/components/List/index.tsx
@@ -1,27 +1,31 @@
 import './styles.less'
 import React from 'react'
-import { Result, Row} from 'antd'
+import { Result, Row } from 'antd'
 import { Card } from '../Card'
 import { useBeers } from '../../context/beers'
+import { useSearch } from '../../context/search'
 import { beer } from '../../types/beer'
 
 export const List: React.FC = () => {
   const { beers } = useBeers()
-  if (beers.length > 0)
+  const { query } = useSearch()
+
+  if (beers.length == 0 && query)
     return (
-      <Row gutter={[16, 16]} >
-        {beers.map((beer : beer) => (
-          <Card key={beer.id} beer={beer} />
-        ))}
+      <Row className="not-found">
+        <Result
+          status="500"
+          title={<span> Beer not found</span>}
+          subTitle="Sorry, we can not found this beer name."
+        />
       </Row>
     )
+
   return (
-    <Row className="not-found">
-      <Result
-        status="500"
-        title={<span> Beer not found</span>}
-        subTitle="Sorry, we can not found this beer name."
-      />
+    <Row gutter={[16, 16]}>
+      {beers.map((beer: beer) => (
+        <Card key={beer.id} beer={beer} />
+      ))}
     </Row>
   )
 }
